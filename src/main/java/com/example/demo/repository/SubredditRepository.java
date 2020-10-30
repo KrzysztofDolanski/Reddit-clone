4d07f8fca0eead76b22a1d1cbd1dc2483578c2c3
KrzysztofDolanski <k.dolanski@wp.pl>
2020-10-30 11:19:21 +0100

    Initial repositories and set database properties in application.properties

--- src/main/java/com/example/demo/repository/SubredditRepository.java
+++ src/main/java/com/example/demo/repository/SubredditRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public interface SubredditRepository {
+import com.example.demo.model.Subreddit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubredditRepository extends JpaRepository<Subreddit, Long> {
 }
--- src/main/java/com/example/demo/repository/VoreRepository.java
+++ src/main/java/com/example/demo/repository/VoreRepository.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository;
 
-public interface VoreRepository {
+import com.example.demo.model.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoreRepository extends JpaRepository<Vote, Long> {
 }
