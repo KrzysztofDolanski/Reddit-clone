d725a8ce3d670a432f2b3a23846b7d4f3e3fa18b
KrzysztofDolanski <k.dolanski@wp.pl>
2020-11-01 12:46:23 +0100

    Mapping class Post

---
+++ src/main/java/com/example/demo/controller/PostController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+@AllArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity createPost(@RequestBody PostRequest postRequest){
+        postService.save(postRequest);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public List<PostResponse> getPost(@PathVariable Long id){
+        return postService.getPost(id);
+    }
+
+    @GetMapping("/by-subreddit/{id}")
+    public List<PostResponse> getPostsBySubreddit(Long id){
+        return postService.getPostsBySubreddit(id);
+    }
+
+    @GetMapping("/")
+    public List<PostResponse> getAllPosts(){
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("by-user/{name}")
+    public List<PostResponse> getPostsByUsername(String username){
+        return postService.getPostsByUsername(username);
+    }
+}
--- src/main/java/com/example/demo/dto/PostRequest.java
+++ src/main/java/com/example/demo/dto/PostRequest.java
@@ -1,20 +1,18 @@
 package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-public class SubredditDto {
+public class PostRequest {
 
-    private Long id;
-    private String name;
+    private Long postId;
+    private String subredditName;
+    private String postName;
+    private String url;
     private String description;
-    private Integer numberOfPosts;
 
 }
-
