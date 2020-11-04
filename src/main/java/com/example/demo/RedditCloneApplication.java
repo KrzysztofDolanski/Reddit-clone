08d6120cdfb89569d1be527048ebc6274475557f
KrzysztofDolanski <k.dolanski@wp.pl>
2020-11-04 08:41:40 +0100

    Swagger added

--- src/main/java/com/example/demo/RedditCloneApplication.java
+++ src/main/java/com/example/demo/RedditCloneApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
@@ -7,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-//@Import(SwaggerConfiguration.class)
+@Import(SwaggerConfiguration.class)
 public class RedditCloneApplication {
 
     public static void main(String[] args) {
